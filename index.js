6c77558190da424aa86488f43bc6cbcfcfe688c2
sagar parise <sagarparise01@gmail.com>
2023-11-05 10:46:59 +0530

    google clone

---
+++ index.js
@@ -0,0 +1,55 @@
+let columns = 26;
+let rowsNo = 100;
+const header = document.querySelector(".header");
+const sno = document.querySelector(".sno");
+const main = document.querySelector(".main");
+
+function createHeaderCells()
+{
+  //header cells here
+  for(let i=0; i<=columns; i++)
+  {
+    let headerCell = document.createElement("div");
+    headerCell.className = "header-cell";
+    if(i!==0)
+    {
+        headerCell.innerText = String.fromCharCode(64+i);
+    }
+    header.appendChild(headerCell);
+  }
+}
+function createSeriesNumberCells()
+{
+    for(let i=1; i<=rowsNo; i++)
+    {
+        let snoCell = document.createElement("div");
+        snoCell.className = "sno-cell";
+        snoCell.innerText = i;
+        sno.appendChild(snoCell);
+    }
+}
+ function createMainRow(){
+  
+  
+    let row = document.createElement("div");
+    row.className = "main-row";
+      for(let j=1; j<=columns; j++)
+      {
+        let mainCell = document.createElement("div");
+        mainCell.className = "main-cell";
+        mainCell.contentEditable = true;
+        row.appendChild(mainCell);
+      }
+    main.appendChild(row);
+  
+ }
+  function buildMainSection()
+  {
+    for(let i=1; i<=rowsNo; i++)
+    {
+      createMainRow();
+    }
+  }
+createHeaderCells();
+createSeriesNumberCells();
+buildMainSection();
